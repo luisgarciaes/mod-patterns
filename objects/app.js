4343bbaedb93dea27fec4da38f63c615386db2da
ruruisu <zaddozgl@gmail.com>
2022-02-27 18:29:32 -0600

    client address

--- objects/app.js
+++ objects/app.js
@@ -1,6 +1,14 @@
 let client = {
     name: 'Luis',
-    lastName:'García'
+    lastName:'García',
+    getName: function(){
+        return `${this.name} `+ `${this.lastName}`
+    },
+    address: {
+        street: 'Abasolo 849',
+        colony: 'El Moralete',
+        postalCode: 2860,
+        municipality:'Colima'
+    }
 }
-console.log(client.name)
-console.log(client.lastName)
+console.log(client.address)
